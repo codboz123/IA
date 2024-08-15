9085670b8058209bade961e0eb75f706a507e14a
codboz123 <106533407+codboz123@users.noreply.github.com>
2024-08-15 09:29:51 -0300

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const pergunta1 = [
+    {
+        enunciado: "Perguntas",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Perguntas",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
+
